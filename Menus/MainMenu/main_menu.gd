6f166c7b007ce5a5751dcfdfa66ad922a7b3d6c5
MaxMorfiX <offmax.me@gmail.com>
2023-05-25 19:41:19 +0200

    made menu

---
+++ Menus/MainMenu/main_menu.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+
+
+func _on_play_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Game/Main/game.tscn")
+
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
