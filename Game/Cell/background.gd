234ef3749271500311596a70ebe4f120b85f8ff3
MaxMorfiX <offmax.me@gmail.com>
2023-06-08 19:09:47 +0200

    forgot to add background.gd to last commit

---
+++ Game/Cell/background.gd
@@ -0,0 +1,9 @@
+extends Sprite2D
+
+@export var size = 190
+@export var radius = 45
+
+func _draw() -> void:
+	var style_box = StyleBoxFlat.new()
+	style_box.set_corner_radius_all(radius)
+	draw_style_box(style_box, Rect2(Vector2(-size/2, -size/2), Vector2(size, size)))
