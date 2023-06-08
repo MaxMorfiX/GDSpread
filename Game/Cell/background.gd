8a3ee6ab5c8d013c12ce22beda23745dcf972093
MaxMorfiX <offmax.me@gmail.com>
2023-06-08 19:18:07 +0200

    accidentally deleted comma in last commit

--- Game/Cell/background.gd
+++ Game/Cell/background.gd
@@ -6,4 +6,4 @@ extends Sprite2D
 func _draw() -> void:
 	var style_box = StyleBoxFlat.new()
 	style_box.set_corner_radius_all(radius)
-	draw_style_box(style_box, Rect2(Vector2(-size/2.0 -size/2.0), Vector2(size, size)))
+	draw_style_box(style_box, Rect2(Vector2(-size/2.0, -size/2.0), Vector2(size, size)))
