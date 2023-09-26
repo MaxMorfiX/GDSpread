00626e45e2f98c6cc7ab6038c4fae8b6a4fb1a5e
MaxMorfiX <offmax.me@gmail.com>
2023-09-26 21:39:30 +0200

    Remade main menu, now it's reacts to the aspect ratio and window size changes. Also, I got some many strange files that I don't remember for what but it looks like they're for some translations

--- Menus/MainMenu/main_menu.gd
+++ Menus/MainMenu/main_menu.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends Node
 
 
 
@@ -6,6 +6,7 @@ func _on_play_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Menus/NewGameEditor/NewGameEditor.tscn")
 
 
-func _on_quit_button_pressed() -> void:
+func _on_exit_button_pressed() -> void:
 	get_tree().quit()
+
 
