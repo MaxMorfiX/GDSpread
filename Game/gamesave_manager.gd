f0af5ba80ee529a48cb3f9615302633dceadcc74
MaxMorfiX <offmax.me@gmail.com>
2024-05-11 20:07:29 +0200

    added save- and load_dict functions to gamesave_manager

--- Game/gamesave_manager.gd
+++ Game/gamesave_manager.gd
@@ -1,11 +1,27 @@
 extends Node
 
+func save_dict(dict: Dictionary, filepath: String = "user://savegame.save") -> void:
+	
+	var json_string: String = JSON.stringify(dict)
+	
+	var save_game: FileAccess = FileAccess.open(filepath, FileAccess.WRITE)
+	save_game.store_line(json_string)
 
-
-
-
-#gamestate = dictionary
-#gamesave = string
-
-
-
+func load_dict(filepath: String) -> Dictionary:
+	
+	if not FileAccess.file_exists(filepath):
+		print("Trying to load a missing savefile! Returning empty dictionary")
+		return {}
+	
+	var save_game = FileAccess.open(filepath, FileAccess.READ)
+	var json_string = save_game.get_as_text()
+	
+	var json = JSON.new()
+	var error = json.parse(json_string)
+	
+	if error == OK:
+		return json.data
+	
+	print("Couldn't parse savedata json while loading gamesave! Returning empty dictionary")
+	
+	return {}
